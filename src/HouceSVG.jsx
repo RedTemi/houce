9ded99cef0f22dcb578845c00d1889706e9134f4
Temi <redtemi@icloud.com>
2023-08-26 19:59:42 +0100

    first draft improvements

---
+++ src/HouceSVG.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+function HouceSVG() {
+  return (
+    <svg
+    xmlns="http://www.w3.org/2000/svg"
+   viewBox="-101 -30 744 950"
+   id='ctasvg'
+  > 
+    <defs>
+      <pattern id="houce" width="100%" height="100%">
+        <image
+          href="IMG_0103.png"
+          x="20px"
+          y="20"
+          width="504.735px"
+          height="880.935px"
+        />
+      </pattern>
+      <path
+        id="Archcurve"
+        fill="none"
+        stroke="none"
+        d="M-0.000,935.000 L-0.000,257.682 C8.113,113.170 129.610,0.072 271.561,-0.000 C407.446,-0.069 525.712,103.470 542.000,241.022 L542.000,935.000 "
+      />
+    </defs>
+    <path
+      fill="url(#houce)"
+      stroke="none"
+      d="M-0.000,935.000 L-0.000,257.682 C8.113,113.170 129.610,0.072 271.561,-0.000 C407.446,-0.069 525.712,103.470 542.000,241.022 L542.000,935.000 "
+    />
+    <text>
+      <textPath
+        href="#Archcurve"
+        fontSize="110.4px"
+        fontFamily="ilyas"
+        startOffset="3.8%"
+      >
+        THE JOURNEY OF FLOW BEGINS WITH THE FIRST PUSH
+      </textPath>
+    </text>
+    <text
+      kerning="auto"
+      fontFamily="Myriad Pro"
+      fill="rgb(0, 0, 0)"
+      fontSize="505.833px"
+      x="-78px"
+      y="960.558px"
+    >
+      <tspan fontSize="150.833px" fill="#000000">&#42;</tspan>
+    </text>
+    <text
+      kerning="auto"
+      fontFamily="Myriad Pro"
+      fill="rgb(0, 0, 0)"
+      fontSize="105.833px"
+      x="550px"
+      y="960.558px"
+    >
+      <tspan fontSize="150.833px" fill="#000000">&#42;</tspan>
+    </text>
+  </svg>
+  )
+}
+
+export default HouceSVG
